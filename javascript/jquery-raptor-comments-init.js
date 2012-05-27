d3b74b798078260b689bbc20d227395a7ef884c1
Michael Robinson <mike@panmedia.co.nz>
2012-05-28 09:10:30 +1200

    Updated comments initialisation

--- javascript/jquery-raptor-comments-init.js
+++ javascript/jquery-raptor-comments-init.js
@@ -1,6 +1,5 @@
 (function($) {
-
-    var raptorCommentOptions = {
+    $('#comment').editor({
         uiOrder: [
             ['viewSource'],
             ['undo', 'redo'],
@@ -13,25 +12,12 @@
         enableUi: false,
         replace: true,
         ui: {
+            viewSource: true,
             textBold: true,
             textItalic: true,
             textUnderline: true,
             textStrike: true,
-            textSub: true,
-            textSuper: true,
-            alignLeft: true,
-            alignRight: true,
-            alignCenter: true,
-            alignJustify: true,
             quoteBlock: true,
-            save: true,
-            cancel: true,
-            floatLeft: true,
-            floatRight: true,
-            floatNone: true,
-            fontSizeInc: true,
-            fontSizeDec: true,
-            hr: true,
             undo: true,
             redo: true,
             link: true,
@@ -40,18 +26,19 @@
             listOrdered: true,
             tagMenu: true
         },
+        disablePlugins: [
+            'unsavedEditWarning'
+        ],
         plugins: {
             dock: {
                 docked: true,
                 dockToElement: true
             },
             save: false,
-            unsavedEditWarning: false
+            unsavedEditWarning: false,
+            placeholder: {
+                content: 'What have you got to say?'
+            }
         }
-    };
-
-    $(function(){
-        $('#comment').editor(raptorCommentOptions);
     });
-
 })(jQuery);
