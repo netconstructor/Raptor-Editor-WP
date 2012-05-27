d0bba150596ed4eca476cd06d00853751833e90d
Michael Robinson <mike@panmedia.co.nz>
2012-05-28 09:10:48 +1200

    Added comment to save

--- save.php
+++ save.php
@@ -1,12 +1,12 @@
 <?php
 
 // If post content data isn't set
-if (!isset($_POST['raptor-post-content']) 
+if (!isset($_POST['raptor-post-content'])
     // Or it is falsy
     || !$_POST['raptor-post-content']
     // Or json_decode can't decode it successfully
     || $posts = json_decode($_POST['raptor-post-content'])) {
     foreach ($posts as $id => $post) {
-
+        // @todo actually save
     }
 }
