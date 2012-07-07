f06d06a4d131355a28137e5c58aa4308e283b5a1
Michael Robinson <mike@pagesofinterest.net>
2012-07-08 00:30:26 +1200

    Updated admin view.

--- views/admin/index.php
+++ views/admin/index.php
@@ -4,38 +4,49 @@
 
         <form method="post" action="" autocomplete="off">
 
-            <?php $this->saveOptions(); ?>
-            <?php settings_fields(self::RAPTOR_SETTINGS); ?>
+            <?php $this->options->save(); ?>
+            <?php settings_fields(RaptorOptions::OPTIONS); ?>
 
-            <table class="form-table">
-                <tr valign="top">
-                    <th scope="row">
-                        <label for="raptor-allow-in-place-editing">Allow in place editing</label>
-                    </th>
-                    <td>
-                        <input id="raptor-allow-in-place-editing" type="checkbox" value="1" name="<?php echo self::INDEX_ALLOW_IN_PLACE_EDITING; ?>" <?php if ($options[self::INDEX_ALLOW_IN_PLACE_EDITING]): ?>checked="checked"<?php endif; ?> />
-                    </td>
-                </tr>
-
-                <tr valign="top">
-                    <th scope="row">
-                        <label for="raptor-raptorize-quickpress">Raptorize Quickpress</label>
-                    </th>
-                    <td>
-                        <input id="raptor-raptorize-quickpress" type="checkbox" value="1"name="<?php echo self::INDEX_RAPTORIZE_QUICKPRESS; ?>" <?php if ($options[self::INDEX_RAPTORIZE_QUICKPRESS]): ?>checked="checked"<?php endif; ?> />
-                    </td>
-                </tr>
-
-                <tr valign="top">
-                    <th scope="row">
-                        <label for="raptor-admin-post-editing">Raptorize admin post editing</label>
-                    </th>
-                    <td>
-                        <input id="raptor-admin-post-editing" type="checkbox" value="1" name="<?php echo self::INDEX_RAPTORIZE_ADMIN_EDITING; ?>" <?php if ($options[self::INDEX_RAPTORIZE_ADMIN_EDITING]): ?>checked="checked"<?php endif; ?> />
-                    </td>
-                </tr>
-            </table>
-
+            <div id="raptor-options-tabs">
+                <ul>
+                    <li><a href="#raptor-options-general">General</a></li>
+                    <li><a href="#raptor-options-editor">Editor</a></li>
+                </ul>
+                <!-- General -->
+                <div id="raptor-options-general">
+                    <strong>Use Raptor Editor to</strong>:
+                    <br/>
+                    <label for="raptor-allow-in-place-editing">
+                        <input id="raptor-allow-in-place-editing" type="checkbox" value="1" name="<?php echo RaptorOptions::INDEX_ALLOW_IN_PLACE_EDITING; ?>" <?php if ($this->options->allowInPlaceEditing()): ?>checked="checked"<?php endif; ?> />
+                        Edit posts on the front end
+                    </label>
+                    <br/>
+                    <label for="raptor-raptorize-quickpress">
+                        <input id="raptor-raptorize-quickpress" type="checkbox" value="1"name="<?php echo RaptorOptions::INDEX_RAPTORIZE_QUICKPRESS; ?>" <?php if ($this->options->raptorizeQuickpress()): ?>checked="checked"<?php endif; ?> />
+                        Edit posts on the dashboard (quickpress)
+                    </label>
+                    <br/>
+                    <label for="raptor-admin-post-editing">
+                        <input id="raptor-admin-post-editing" type="checkbox" value="1" name="<?php echo RaptorOptions::INDEX_RAPTORIZE_ADMIN_EDITING; ?>" <?php if ($this->options->raptorizeAdminEditing()): ?>checked="checked"<?php endif; ?> />
+                        Edit posts on the main admin page
+                    </label>
+                </div>
+                <div id="raptor-options-editor">
+                    <label for="raptor-admin-resize-images">
+                        <input id="raptor-admin-resize-images" type="checkbox" value="1" name="<?php echo RaptorOptions::RESIZE_IMAGES_AUTOMATICALLY; ?>" <?php if ($this->options->resizeImagesAutomatically()): ?>checked="checked"<?php endif; ?> />
+                        Automatically resize inserted image when the image is too large (<em>Editing-in-place only</em>)
+                    </label>
+                </div>
+            </div>
+            <script type="text/javascript">
+                jQuery(function() {
+                    jQuery('#raptor-options-tabs').tabs({
+                        cookie: {
+                            expires: 10
+                        }
+                    });
+                });
+            </script>
             <p class="submit">
                 <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
             </p>
@@ -45,7 +56,7 @@
     <div class="raptor-settings-information">
         <h2>WP Raptor's Roots</h2>
         <p>
-            Plugin created by Michael Robinson, who lives at <a target="_blank" href="http://pagesofinterest.net/?utm_source=wp-raptor&utm_medium=admin-index&utm_content=michael-robinson&utm_campaign=wp-raptor" title="Michael Robinson!">pagesofinterest.net</a>, and uses Twitter.
+            Wordpress Plugin created by Michael Robinson, who lives at <a target="_blank" href="http://pagesofinterest.net/?utm_source=wp-raptor&utm_medium=admin-index&utm_content=michael-robinson&utm_campaign=wp-raptor" title="Michael Robinson!">pagesofinterest.net</a>, and uses Twitter.
             <br/>
             <br/>
             <a href="https://twitter.com/pagesofinterest" class="twitter-follow-button" data-show-count="false">Follow @pagesofinterest</a>
